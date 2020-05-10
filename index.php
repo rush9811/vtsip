65a378b0a8e8757d528f2643c1f92e89995c2fc1
Daniel <daniel.veres@open.telekom.rs>
2020-05-10 17:09:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.php';
+?>
+<div id="particles-js">
+
+</div>
+<?php
+include 'footer.php';
+?>
---
+++ nav.php
@@ -0,0 +1,6 @@
+                            <li class="selected"><a href="">Nav</a></li>
+                            <li><a href="">Nav</a></li>
+                            <li><a href="">Nav</a></li>
+                            <li><a href="">Nav</a></li>
+                            <li><a href="">Nav</a></li>
+                            <li><a href="">Nav</a></li>
